88d7949b3945af468aafeeea5a42409919491275
Matteo Sala <matteo111997@gmail.com>
2024-05-15 12:58:56 +0200

    Onjects and arrays

--- JS/JS-#1/main.js
+++ JS/JS-#1/main.js
@@ -1,5 +1,20 @@
 console.log('Hello World');
 
+let firstName = 'Matteo', lastName = 'Sala'
+
+console.log(firstName, lastName);
 
 
 
+// Objects and arrays
+let person = {
+    name: 'Mario',
+    age: 25
+}
+person.name = "Marco"
+
+console.log(person.name)
+
+person['name']= 'Luca'
+
+console.log(person.name)
